e1a6d06092992bb60a727caa183ade8dac75d118
Shaxadhere <shaxad.here@gmail.com>
2021-05-11 21:02:04 +0500

    app.js init

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const expressValidator = require('express-validator')
+require('dotenv').config()
 
+//App
+const app = express()
+
+//Database Connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => {
+    console.log("Connected to database")
+})
+
+//Middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(expressValidator())
+
+//Registering Routes
+
+//Listening to Server
+const port = process.env.PORT || 8000
+app.listen(port, (port) => {
+    console.log(`Listening to server on ${port}`)
+})
